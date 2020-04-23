1e31c6fae435cbf20877ebcde98baa1ead3056a1
Dean Mulrooney <mulrooney.dean@gmail.com>
2020-04-23 16:24:55 +0100

    Added listview

--- app/src/main/java/com/example/four2floor/activities/MainActivity.java
+++ app/src/main/java/com/example/four2floor/activities/MainActivity.java
@@ -2,20 +2,43 @@ package com.example.four2floor.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.four2floor.adapters.PostsAdapter;
+import com.example.four2floor.managers.PostManager;
+import com.example.four2floor.managers.listeners.OnDataChangedListener;
+import com.example.four2floor.model.Post;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import com.example.four2floor.R;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView postsListView;
+    private PostsAdapter postsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        postsListView = (ListView)findViewById(R.id.postsListView);
+        postsAdapter = new PostsAdapter();
+        postsListView.setAdapter(postsAdapter);
+
+        OnDataChangedListener<Post> onPostsDataChangedListener = new OnDataChangedListener<Post>() {
+            @Override
+            public void onListChanged(List<Post> list) {
+                postsAdapter.setList(list);
+            }
+        };
+
+        PostManager.getInstance(getApplicationContext()).getPosts(onPostsDataChangedListener);
 
         FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.addNewPostFab);
 
